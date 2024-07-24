6006303fae6da01bde0171164a4716138e5defdf
Suraj Mandal <surajmandalcell@gmail.com>
2024-07-24 13:10:28 -0400

    chore: Add Card component for displaying styled cards

---
+++ components/Card.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode } from "react";
+import { View, StyleSheet, ViewStyle } from "react-native";
+
+interface CardProps {
+  children: ReactNode;
+  style?: ViewStyle;
+}
+
+const Card: React.FC<CardProps> = ({ children, style }) => {
+  return <View style={[styles.card, style]}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 8,
+    padding: 16,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+});
+
+export default Card;
--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren, ReactElement } from "react";
-import { StyleSheet, useColorScheme } from "react-native";
+import { StyleSheet, useColorScheme, ViewStyle } from "react-native";
 import Animated, {
   interpolate,
   useAnimatedRef,
@@ -15,13 +15,15 @@ type Props = PropsWithChildren<{
   extra_padding?: boolean;
   headerImage?: ReactElement;
   headerBackgroundColor?: { dark: string; light: string };
+  container_style?: ViewStyle;
 }>;
 
 export default function ParallaxScrollView({
   children,
-  extra_padding = false,
   headerImage,
+  extra_padding = false,
   headerBackgroundColor,
+  container_style,
 }: Props) {
   const colorScheme = useColorScheme() ?? "light";
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
@@ -49,7 +51,7 @@ export default function ParallaxScrollView({
   });
 
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView style={{ ...styles.container, ...container_style }}>
       <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
         {headerImage && headerBackgroundColor && (
           <Animated.View
@@ -82,8 +84,8 @@ const styles = StyleSheet.create({
     flex: 1,
     gap: 16,
     padding: 32,
-    paddingHorizontal: 24,
     overflow: "hidden",
+    paddingHorizontal: 10,
   },
   content_60: {
     paddingTop: 70,
